b0c4c25edaa58ac4f8121d6b8f551e7974fd8d46
İbrahim ATAY <contact@ibrahimatay.com>
2020-03-15 16:35:21 +0300

    Static local functions (csharp-8)

---
+++ StaticLocalFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StaticLocalFunctions
+{
+    /// <summary>
+    /// Static local functions
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8#static-local-functions
+    /// https://github.com/dotnet/csharplang/blob/master/proposals/csharp-8.0/static-local-functions.md
+    /// https://github.com/dotnet/csharplang/issues/1565
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            int y = 10;
+
+            Console.WriteLine(Extraction(x,y));
+            
+            static int Extraction(int val1, int val2) => val1 - val2;
+        }
+
+        int Foo()
+        {
+            int x = 5;
+            int y = 12;
+
+            return Sum(x, y);
+            
+            static int Sum(int val1, int val2) => val1 + val2;
+            
+            //Doing so generates CS8421, "A static local function can't contain a reference to <variable>."
+            //static School (string name) => new School() {SchoolName = name};
+        }
+    }
+
+    class School
+    {
+        public string SchoolName { get; set; }
+    }
+}
