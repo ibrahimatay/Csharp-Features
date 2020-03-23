12c0d3a494b328651e4eb6beb195b7e167f1699c
İbrahim ATAY <contact@ibrahimatay.com>
2020-03-23 15:31:46 +0300

    Numeric literal syntax improvements (csharp-7.0)

---
+++ NumericLiteralSyntaxImprovements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumericLiteralSyntaxImprovements
+{
+    /// <summary>
+    /// Numeric literal syntax improvements
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7#numeric-literal-syntax-improvements
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Without Using digit separators 
+            long x = 100000022200000202; 
+            long y = 10000000020; 
+  
+            Console.WriteLine($"X: {x}"); 
+            Console.WriteLine($"Z: {y}");
+            
+            // Using digit separators 
+            long num1 = 1_00_10_00_00_00; 
+            var num2 = 0b_010_000_000_000_000_000_000_000_000; 
+            long num3 = 1_00_00_00_00_00_00; 
+            var num4 = 0b_1_1000_0000_1000_0000_0011_0000_0000_1000_0001;
+            var num5 = 0_3.14_00;
+            
+            Console.WriteLine($"Num1: {num1}"); 
+            Console.WriteLine($"Num2: {num2}"); 
+            Console.WriteLine($"Num3: {num3}"); 
+            Console.WriteLine($"Num4: {num4}");
+            Console.WriteLine($"Num4: {num5}");
+        }
+    }
+}
