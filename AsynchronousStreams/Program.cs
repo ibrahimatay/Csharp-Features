c80ea7ed1aa0c3e500cfe36e387124c1fd29fb89
İbrahim ATAY <contact@ibrahimatay.com>
2020-04-19 15:10:40 +0300

    Async Streams (csharp-8)

---
+++ AsynchronousStreams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreams
+{
+    /// <summary>
+    /// Async Streams
+    /// https://blog.jetbrains.com/dotnet/2019/09/16/async-streams-look-new-language-features-c-8/
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/generate-consume-asynchronous-stream
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cancellationToken = new CancellationTokenSource(millisecondsDelay: 1000);
+            await foreach (var result in GetTopSearchResults("dotnet").WithCancellation(cancellationToken.Token))
+            {
+                Console.WriteLine(result);
+            }
+        }
+        
+        static async IAsyncEnumerable<string> GetTopSearchResults(string term, [EnumeratorCancellation]CancellationToken token = default)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                using var client = new HttpClient();
+                yield return await client.GetStringAsync($"https://www.google.com?q={term}");
+                yield return await client.GetStringAsync($"https://www.bing.com?q={term}"); 
+            }
+        }
+    }
+}
