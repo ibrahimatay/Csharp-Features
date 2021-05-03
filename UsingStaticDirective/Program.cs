b713f39be9b88c6a0da8be4e90e5537e3cf5cc65
İbrahim Atay <contact@ibrahimatay.com>
2021-05-03 10:25:15 +0300

    Static namespace In (C# 6.0)

--- UsingStaticDirective/Program.cs
+++ UsingStaticDirective/Program.cs
@@ -5,6 +5,10 @@ using static UsingStaticDirective.CommonUtil;
 
 namespace UsingStaticDirective
 {
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-static
+    /// https://stackoverflow.com/questions/31852389/how-do-i-use-the-c6-using-static-feature
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
