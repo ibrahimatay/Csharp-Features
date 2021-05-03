c4caec6f0b2f21bc6c3e63109587aae9ef6cb920
İbrahim Atay <contact@ibrahimatay.com>
2021-05-03 10:22:17 +0300

    Static Namespace In C# 6.0

---
+++ UsingStaticDirective/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+using static UsingStaticDirective.CommonUtil;
+
+namespace UsingStaticDirective
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Sqrt(144));
+            WriteLine("Hello World!");
+            WriteLine(GetMessage());
+        }
+    }
+
+    static class CommonUtil
+    {
+        public static string GetMessage() => "Test message from CommonUtil";
+    }
+}
