88fdbeb6e9fa19c7ac1612f0dab09ff7ca7057c6
İbrahim Atay <contact@ibrahimatay.com>
2021-05-20 11:58:44 +0300

    Top-level Statements (csharp-9)

---
+++ TopLevelStatements/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Hello Console!");
+Console.WriteLine(GetHelloStatement("İbrahim"));
+
+static string GetHelloStatement(string name)=> $"Hello {name}, \nWelcome Top-level Statements feature.";
