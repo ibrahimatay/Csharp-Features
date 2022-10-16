2c1406c8e56086e3a4af15d5001205242b32e2d1
İbrahim ATAY <contact@ibrahimatay.com>
2022-10-16 16:58:31 +0300

    File-local types (csharp-11)

---
+++ FileLocalTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/file-local-types
+
+namespace FileLocalTypes;
+
+Widget widget = new Widget();
+
+
