6e31d127997dabc05a85cdf64c4f6f4ab1bfc380
İbrahim ATAY <contact@ibrahimatay.com>
2022-10-16 14:40:20 +0300

    ListPatternsMatching (csharp-11)

--- ListPatternsMatching/Program.cs
+++ ListPatternsMatching/Program.cs
@@ -1,17 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// https://thecodeblogger.com/2022/09/19/c-11-introducing-list-patterns-matching/
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/pattern-matching
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/pattern-matching
 // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/patterns
-// https://timdeschryver.dev/blog/pattern-matching-examples-in-csharp#relational-patterns
-// https://stackoverflow.com/questions/70901344/c-sharp-pattern-match-arrays
-// https://gsferreira.com/archive/2022/a-gentle-introduction-to-csharp-11-list-patterns/
-// https://endjin.com/blog/2022/02/pattern-matching-in-csharp
-// https://www.youtube.com/watch?v=z7bW83Xr1R0&ab_channel=NickChapsas
 
 int[] fibonacci = { 1, 2, 3, 5, 8 };
 
-// result is false, last number is not matching
-var result = fibonacci is [1, 2, 3, 5, 9];
+bool result = false;
 
-Console.WriteLine("Hello, World!");
+// result is false, last number is not matching
+result = fibonacci is [1, 2, 3, 5, 9];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, 2, 3, 5, 9] = {result}");
+
+// result is false, elements are not at same positions
+result = fibonacci is [8, 1, 2, 3, 5];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [8, 1, 2, 3, 5] = {result}");
+
+// result is false, length is not matching
+result = fibonacci is [1, 2, 3, 5];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, 2, 3, 5] = {result}");
+
+// result is true, elements, positions and length is matching
+result = fibonacci is [1, 2, 3, 5, 8];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, 2, 3, 5, 8] = {result}");
+
+// result is false, length not matching
+result = fibonacci is [_, _, 3, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [_, _, 3, _] = {result}");
+
+// result is false, 3 is not at same position
+result = fibonacci is [_, _, _, 3, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [_, _, _, 3, _] = {result}");
+
+// result is false, length is matching but 2 and 3 not at same positions
+result = fibonacci is [2, _, 3, _, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [2, _, 3, _, _] = {result}");
+
+// result is true, single element and its position and length is matching
+result = fibonacci is [1, _, _, _, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, _, _, _, _] = {result}");
+
+// result is true, multiple elements and their positions and length is matching
+result = fibonacci is [1, _, 3, _, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, _, 3, _, _] = {result}");
+
+// result is true, as fibonacci ends with element 8
+result = fibonacci is [.., 8];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, _, 3, _, _] = {result}");
+
+// result is false, 3 is not second last element
+result = fibonacci is [.., 3, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, _, 3, _, _] = {result}");
+
+// result is true, as sequence begins with 1 and ends with 8
+result = fibonacci is [1, .., 8];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, .., 8] = {result}");
+
+// result is true, as 1 is first element
+result = fibonacci is [1, ..];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [1, ..] = {result}");
+
+// result is false, as fibonacci ends with element 8
+result = fibonacci is [..,  <8];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [..,  <8] = {result}");
+
+// result is true, 3 is third element from the ending
+result = fibonacci is [.., >= 3, _, _];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [.., >= 3, _, _] = {result}");
+
+// result is false, as first element is not greater than 1
+result = fibonacci is [>1, .., 8];
+Console.WriteLine($"[1, 2, 3, 5, 9] is [>1, .., 8] = {result}");
