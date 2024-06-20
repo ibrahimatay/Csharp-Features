f6896e8fba4de1b27e1a0407684ddba5f15bc1c5
İbrahim ATAY <atay363@gmail.com>
2024-06-20 14:13:59 +0000

    The lock statement (csharp-13)

--- TheLockStatement/Program.cs
+++ TheLockStatement/Program.cs
@@ -2,18 +2,33 @@
 // The lock statement - ensure exclusive access to a shared resource
 // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/lock 
 
+// Lock Class
+// src/libraries/System.Private.CoreLib/src/System/Threading/Lock.cs
+// https://learn.microsoft.com/en-us/dotnet/api/system.threading.lock?view=net-9.0
+
 Singleton instance = Singleton.Instance;
 
 Console.WriteLine(instance.CurrentDateTime);
 
-Task.Factory.StartNew(()=>{
+// What is the difference between Task.Run() and Task.Factory.StartNew()
+// https://stackoverflow.com/questions/38423472/what-is-the-difference-between-task-run-and-task-factory-startnew
+
+Task.Factory.StartNew(() =>
+{
     Singleton instance2 = Singleton.Instance;
     Console.WriteLine(instance2.CurrentDateTime);
 
-    new Task(()=>{
+    new Task(() =>
+    {
         Thread.Sleep(10000);
     }, TaskCreationOptions.AttachedToParent).Start();
-    
+
+    new Task(() =>
+    {
+        Thread.Sleep(5000);
+        Singleton instance2 = Singleton.Instance;
+        Console.WriteLine(instance2.CurrentDateTime);
+    }, TaskCreationOptions.AttachedToParent).Start();
 });
 
 
@@ -21,27 +36,23 @@ public class Singleton
 {
     private readonly System.Threading.Lock _lock = new();
     private static Singleton instance = null;
-
     public DateTime CurrentDateTime => _currentDateTime;
     private DateTime _currentDateTime;
-
-    private  Singleton() { }
-
-    public static Singleton Instance 
+    private Singleton() { }
+    public static Singleton Instance
     {
-        get 
+        get
         {
-            if(_lock) 
+            if (_lock)
             {
-                if (instance == null) 
+                if (instance == null)
                 {
                     instance = new();
                     instance._currentDateTime = DateTime.Now;
                 }
-            }  
+            }
 
-            return instance;       
-        }       
+            return instance;
+        }
     }
-
 }
